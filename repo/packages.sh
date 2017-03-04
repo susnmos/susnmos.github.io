718a65c744884be5c9c8eb8649980ef06eebdcca
susnm <wwzmzs@gmail.com>
2017-03-05 00:28:26 +0800

    no message

--- repo/packages.sh
+++ repo/packages.sh
@@ -1,4 +1,4 @@
 dpkg-deb -bZgzip projects/NsB debs
 dpkg-deb -bZgzip projects/Nicon debs
-dpkg-deb -bZgzip projects/WXHook debs
-dpkg-deb -bZgzip projects/RevealLoader debs
+dpkg-deb -bZgzip /Users/susnm/Code/WXHook/WXHook/Package debs
+dpkg-deb -bZgzip /Users/susnm/Code/RevealLoader2/Reveal/Reveal/Package debs
