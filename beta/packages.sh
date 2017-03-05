88ace0ac925f9bcc2124666f51effa56133a74ea
susnm <wwzmzs@gmail.com>
2017-03-05 09:21:39 +0800

    Add beta source

--- beta/packages.sh
+++ beta/packages.sh
@@ -1,4 +1,2 @@
-#dpkg-deb -bZgzip projects/NsB debs
-#dpkg-deb -bZgzip projects/Nicon debs
 dpkg-deb -bZgzip /Users/susnm/Code/WXHook/WXHook/Package debs
 dpkg-deb -bZgzip /Users/susnm/Code/RevealLoader2/Reveal/Reveal/Package debs
