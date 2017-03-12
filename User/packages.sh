bbd99e1dc5816522427f878265693d9461e9f967
susnm <wwzmzs@gmail.com>
2017-03-12 13:34:56 +0800

    no message

--- User/packages.sh
+++
@@ -1,4 +0,0 @@
-#dpkg-deb -bZgzip projects/NsB debs
-#dpkg-deb -bZgzip projects/Nicon debs
-dpkg-deb -bZgzip /Users/susnm/Code/WXHook/WXHook/Package debs
-dpkg-deb -bZgzip /Users/susnm/Code/RevealLoader2/Reveal/Reveal/Package debs
